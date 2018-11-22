234f8f1b04295b16efc8b83ecfdf18bb4c801704
jsmundi <14978346+jsmundi@users.noreply.github.com>
2018-11-21 21:53:33 -0800

    Fixed layout and page refresh issues.

--- app/imports/api/stuff/stuff.js
+++ app/imports/api/stuff/stuff.js
@@ -3,7 +3,7 @@ import { SimpleSchema } from 'meteor/aldeed:simple-schema';
 
 /* eslint-disable object-shorthand */
 
-export const Stuff = new Mongo.Collection('Stuff');
+export const Stuff = new Mongo.Collection('Add Contact');
 
 /**
  * Create the schema for Stuff
@@ -15,8 +15,8 @@ export const StuffSchema = new SimpleSchema({
     optional: false,
     max: 20,
     autoform: {
-      group: 'Stuff',
-      placeholder: 'Xinghui',
+      group: 'Add Contact',
+      placeholder: 'First Name',
     },
   },
   last: {
@@ -25,8 +25,8 @@ export const StuffSchema = new SimpleSchema({
     optional: false,
     max: 20,
     autoform: {
-      group: 'Stuff',
-      placeholder: 'Zhao',
+      group: 'Add Contact',
+      placeholder: 'Last Name',
     },
   },
   address: {
@@ -35,18 +35,18 @@ export const StuffSchema = new SimpleSchema({
     optional: false,
     max: 100,
     autoform: {
-      group: 'Stuff',
-      placeholder: 'WSU Vancouver',
+      group: 'Add Contact',
+      placeholder: 'Address',
     },
   },
   phone: {
-    label: 'Phone',
+    label: 'Telephone',
     type: String,
     optional: false,
     max: 20,
     autoform: {
-      group: 'Stuff',
-      placeholder: '360-123-4567',
+      group: 'Add Contact',
+      placeholder: 'Telephone',
     },
   },
   email: {
@@ -55,8 +55,8 @@ export const StuffSchema = new SimpleSchema({
     optional: false,
     max: 20,
     autoform: {
-      group: 'Stuff',
-      placeholder: 'x.zhao@wsu.edu',
+      group: 'Add Contact',
+      placeholder: 'Email',
     },
   },
 });
